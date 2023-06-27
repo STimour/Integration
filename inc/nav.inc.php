2b2263da9d447474a3072a3583eb5f877c710501
STimour <st.sayfoutdinov@icloud.com>
2023-06-27 15:02:30 +0200

    mise en page v2

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -21,3 +21,63 @@
               </nav>
         </nav>
     </header>
+
+<!-- ANCHOR 2eme nav -->
+    
+<main class="d-flex flex-row">
+  <section class="col-md-2">
+    <ul class="nav flex-column h-100">
+        <h1>Titre 1</h1>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+      <h1>Titre 2</h1>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+      <h1>Titre 3</h1>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+      <h1>Titre 4</h1>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  </section>
--- index.php
+++ index.php
@@ -2,68 +2,12 @@
 include "inc/header.inc.php";
 include "inc/nav.inc.php";
 ?>
-<main class="d-flex flex-row">
-<section class="col-md-4">
-<ul class="nav flex-column h-100">
-    <h1>Titre 1</h1>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-  <h1>Titre 2</h1>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-  <h1>Titre 3</h1>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-  <h1>Titre 4</h1>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
-</section>
-<section class="col-md-8 text-center h-100">
+
+<section class="col-md-10 text-center h-75">
     <h1>Titre</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-    <section class="d-flex flex-column col-md-8 m-auto">
-        <article class="col-md-8 row m-auto align-items-center">
+    <section class="d-flex flex-column col-md-10 m-auto">
+        <article class="col-md-8 m-auto align-items-center d-flex justify-content-evenly">
             <div class="col-md-6">
                 <img src="assets/img/logo.png" alt="">
             </div>
@@ -71,7 +15,7 @@ include "inc/nav.inc.php";
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
         </article>
-        <article class="col-md-8 row m-auto align-items-center">
+        <article class="col-md-8 m-auto align-items-center d-flex justify-content-evenly">
             <div class="col-md-6">
                 <img src="assets/img/logo.png" alt="">
             </div>
