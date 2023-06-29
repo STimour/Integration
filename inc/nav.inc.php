0ce9fa1a199f8e82507f7ecbac5e6e71cdb7a314
STimour <st.sayfoutdinov@icloud.com>
2023-06-29 14:27:45 +0200

    ajout du readme

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,126 +1,59 @@
-
-<header class="mb-5">
-        <nav>
-            <nav class="navbar navbar-expand-lg bg-black fixed-top">
-                <div class="container-fluid text-light">
-                  <a class="navbar-brand" href="index.php"><img src="../assets/img/logo.png" id="logo" alt="image avec 'HTML' ecrit dessus"></a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-light" aria-current="page" href="index.php">Accueil</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link text-light"href="tableau.php">Classement alphabétique</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link text-light"aria-current="page" href="exemple.php">Exemples</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link text-light"href="source.php">Sources</a>
-                      </li>
-                      <li class="nav-item">
-                      <a class="nav-link text-light" href="#a">A</a>
-                      </li>
-                      
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-        </nav>
-    </header>
-
-<!-- ANCHOR 2eme nav -->
-    
-<main class="d-flex justify-content-evently">
-  <section class="col-md-2 text-center overflow-scroll pt-5 bg-black">
-    <ul class="nav flex-column h-100">
-        <h1 class="text-light"><a class="nav-link text-light" href="#a">A</a></h1>
-        <!-- ANCHOR A -->
-      <li class="nav-item">
-        <a class="nav-link text-light" aria-current="page" href="#azimuth">azimuth</a>
-      </li>
-      <!-- ANCHOR B -->
-      <h1 class="text-light"><a href="#b">B</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#background">background</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#behavior">behavior</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#border" >border</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="bottom">bottom</a>
-      </li>
-      <!-- ANCHOR C -->
-      <h1 class="text-light"><a href="#c">C</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#caption-side">caption-side</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#clear">clear</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#clip">clip</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="color">color</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#content">content</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#counter-increment">counter-increment</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#counter-reset">counter-reset</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#cue">cue</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light" href="#cursor">cursor</a>
-      </li>
-
-      <!-- ANCHOR D -->
-      <h1 class="text-light"><a href="#d">D</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#direction">direction</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#display">display</a>
-      </li>
-
-      <!-- ANCHOR E -->
-      <h1 class="text-light"><a href="#e">E</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#elevation">elevation</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#empty-cells">empty-cells</a>
-      </li>
-    
-
-     <!-- ANCHOR F -->
-     <h1 class="text-light"><a href="#f">F</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#filter">filter</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#float">float</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-light"href="#font">font</a>
-      </li>
-      <!-- ANCHOR H -->
-     <h1 class="text-light"><a href="#h">H</a></h1>
-      <li class="nav-item">
-        <a class="nav-link text-light"aria-current="page" href="#height">height</a>
-      </li>
-    </ul>
-
-  </section>
+<nav class="navbar navbar-expand-lg bg-black border-bottom-dark fixed-top">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="index.php"><img src="../assets/img/logo.png" id="logo" alt="image avec 'HTML' ecrit dessus"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light"href="tableau.php">Classement alphabétique</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light"aria-current="page" href="source.php">Sources</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Background
+          </a>
+          <ul class="dropdown-menu bg-black">
+            <li><a class="dropdown-item text-light link-dark" href="background.php">background</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">background-attachment</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">background-color</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">background-image</a></li>
+            <li><a class="dropdown-item text-light link-dark" href="background.php">background-position</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">background-repeat</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">layer-background-image</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="background.php">layer-background-color</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Border
+          </a>
+          <ul class="dropdown-menu bg-black">
+            <li><a class="dropdown-item text-light link-dark"  href="#">Action</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="#">Another action</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Couleur
+          </a>
+          <ul class="dropdown-menu bg-black">
+            <li><a class="dropdown-item text-light link-dark"  href="#">Action</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="#">Another action</a></li>
+            <li><a class="dropdown-item text-light link-dark"  href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" aria-current="page" href="tableau.php">Tout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -3,37 +3,30 @@ include "inc/header.inc.php";
 include "inc/nav.inc.php";
 ?>
 
-<section class="col-md-10 h-75 overflow-scroll mt-5">
+<section class="col-md-10 mt-5 text-light p-5">
     <div class="m-auto col-md-10">
-        <div class="col-md-10 m-auto">
-            <h1>Définition du mot CSS</h1>
-            <p>
+        <div class="col-md-10 m-auto h-100 mb-5">
+            <h1 class="text-light">Définition du mot CSS</h1>
+            <p class="text-light">
                 Le CSS pour Cascading Style Sheets, est un langage informatique utilisé sur Internet pour la mise en forme de fichiers et de pages HTML. On le traduit en français par feuilles de style en cascade.
                 <br>
                 Apparu dans les années 1990, le CSS se présente comme une alternative à la mise en forme via des balises, notamment HTML. Un peu plus complexe à maîtriser, il permet un gain de temps considérable dans la mise en forme d'une page web par rapport à ces balises. Grâce au CSS, vous pouvez en effet appliquer des règles de mise en forme (titrage, alignement, polices, couleurs, bordures, etc.) à plusieurs documents simultanément.
             </p>
         </div>
-        <section class="d-flex flex-column col-md-10 m-auto">
-            <article class="col-md-10 m-auto align-items-center d-flex flex-wrap justify-content-evenly mb-2 border border-primary-subtle">
-                    <h2></h2>
-                    <table class="table table-hover">
-                        <tr class="table-secondary">
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                        <tr>
-                            <td class="table-secondary">...</td>
-                            <td class="table-secondary">...</td>
-                            <td class="table-secondary">...</td>
-                        </tr>
-                    </table>
-            </article>
-            <article class="col-md-10 m-auto align-items-center d-flex flex-wrap justify-content-evenly mb-2">
-                    <img  class="col-md-6" src="assets/img/logo.png" alt="">
-                    <p class="col-md-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            </article>
-            </article>
+        <section class="d-flex flex-column align-items-center h-100 d-inline-block col-md-10 m-auto mb-5">
+            <h2 class="text-light">azimuth</h2>
+            <table class="table table-hover">
+                <tr class="table-dark">
+                    <th scope="col">Propriétés</th>
+                    <th scope="col">Descriptifs</th>
+                    <th scope="col">Familles</th>
+                </tr>
+                <tr>
+                    <td class="table-dark">azimuth</td>
+                    <td class="table-dark">Provenance horizontale du son.</td>
+                    <td class="table-dark">Son</td>
+                </tr>
+            </table>
         </section>
     </div>
 </section>
